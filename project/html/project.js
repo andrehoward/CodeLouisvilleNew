d2d29534a20d82b100ed0b3b53c60ddaf66be160
andrehoward <andre.howard40@yahoo.com>
2016-07-19 07:39:48 -0400

    change to page

--- project/html/project.js
+++
@@ -1,9 +0,0 @@
-var person = prompt("Please enter your name", "Andre' Howard");
-
-// add response to enter name. maybe hello name + welcome
-
-alert("Hello\nHow are you?");
-
-//add response to hello how are you
-
-
